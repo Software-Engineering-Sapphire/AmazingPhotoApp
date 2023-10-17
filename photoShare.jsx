15faadec8ba84468334130345ea6e545b2f512d3
Sakarmit <40968890+Sakarmit@users.noreply.github.com>
2023-10-17 14:42:07 -0400

    Remove Message on right when no user is selected

--- photoShare.jsx
+++ photoShare.jsx
@@ -4,7 +4,7 @@ import {
     HashRouter, Route, Switch
 } from 'react-router-dom';
 import {
-    Grid, Typography, Paper
+    Grid, Paper
 } from '@mui/material';
 import './styles/main.css';
 
@@ -44,25 +44,7 @@ class PhotoShare extends React.Component {
                         <Grid item sm={9}>
                             <Paper className="main-grid-item">
                                 <Switch>
-                                    <Route exact path="/"
-                                           render={() => (
-                                               <Typography variant="body1">
-                                                   Welcome to your photosharing app! This
-                                                   <a
-                                                   href="https://mui.com/components/paper/">Paper
-                                                   </a>
-                                                   component
-                                                   displays the main content of the application. The {"sm={9}"} prop in
-                                                   the <a href="https://mui.com/components/grid/">Grid</a> item
-                                                   component makes it responsively
-                                                   display 9/12 of the window. The Switch component enables us to
-                                                   conditionally render different
-                                                   components to this part of the screen. You don&apos;t need to display
-                                                   anything here on the homepage,
-                                                   so you should delete this Route component once you get started.
-                                               </Typography>
-                                           )}
-                                    />
+                                    <Route exact path="/"/>
                                     {/* :userId is a placeholder for a user ID. userId is passed as props to the UserDetail component. */}
                                     <Route path="/users/:userId"
                                            render={props => (
